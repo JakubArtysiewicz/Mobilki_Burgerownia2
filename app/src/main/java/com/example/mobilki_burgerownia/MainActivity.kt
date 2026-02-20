94412078ecbcfdb14b1fec03b65f576c454defb3
jakub.artysiewicz_in <jakub.artysiewicz@infotech.edu.pl>
2026-02-20 11:42:26 +0100

    zrobienie działanie, zrobić jeszcze odznaczanie buttonów

--- app/src/main/java/com/example/mobilki_burgerownia/MainActivity.kt
+++ app/src/main/java/com/example/mobilki_burgerownia/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.mobilki_burgerownia
 
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.MotionEvent
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.textfield.TextInputEditText
+import kotlin.collections.forEach
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,5 +25,60 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         val iloscBurgerow = this.findViewById<TextInputEditText>(R.id.ilosc_burgerow_input);
+
+        val malyButton = this.findViewById<RadioButton>(R.id.malyButton);
+        val duzyButton = this.findViewById<RadioButton>(R.id.duzyButton);
+        val serButton = this.findViewById<RadioButton>(R.id.serButton);
+        val beconButton = this.findViewById<RadioButton>(R.id.beconButton);
+
+        malyButton.tag = 10;
+        duzyButton.tag = 15;
+        serButton.tag = 2;
+        beconButton.tag = 3;
+        val listOfMainRadios = listOf<RadioButton>(malyButton,duzyButton);
+        val listOfRadio = listOf<RadioButton>(malyButton,duzyButton,serButton,beconButton);
+
+//        listOfRadio.forEach { radio ->
+//            var wasChecked = false;
+//            radio.setOnTouchListener { _, event ->
+//                if (event.action == MotionEvent.ACTION_DOWN) {
+//                    wasChecked = radio.isChecked
+//                }
+//                false
+//            }
+//            radio.setOnClickListener {
+//                if (wasChecked) {
+//                    radio.isChecked = false
+//                }
+//            }
+//        }
+
+        fun alert(kwota: Int) {
+            AlertDialog.Builder(this)
+                .setTitle("Kwota")
+                .setMessage("Kwota: ${kwota} zł")
+                .setPositiveButton(
+                    "Ok",
+                    DialogInterface.OnClickListener { dialog, which ->
+                        dialog.dismiss()
+                    })
+                .show()
+        }
+        this.findViewById<Button>(R.id.ObliczButton).setOnClickListener {
+            var podsumowanie = 0;
+            listOfMainRadios.forEach { radio ->
+                if(radio.isChecked){
+                    listOfRadio.forEach{ radio ->
+                        if(radio.isChecked){
+                            podsumowanie += radio.tag as Int;
+                        }
+                    }
+                    alert(podsumowanie)
+                }
+                else{
+                    Toast.makeText(this,"Dodaj Wielkość Burgera", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 }
